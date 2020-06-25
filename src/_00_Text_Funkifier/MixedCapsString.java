584bd5d78d9b9e1b19cf31b19d65f0593b1dac6a
J1a4b4 <46267154+J1a4b4@users.noreply.github.com>
2020-06-25 15:31:49 -0700

    TextFunkifier (IP)
    :smiley:

--- src/_00_Text_Funkifier/MixedCapsString.java
+++ src/_00_Text_Funkifier/MixedCapsString.java
@@ -8,8 +8,17 @@ public class MixedCapsString extends SpecialString{
 
 	@Override
 	public String funkifyText(String s) {
-		
-		return "";
+		String sI = "";
+		for (int i = 0; i < s.length(); i++) {
+			sI = s.substring(i, i + 1);
+			if (i % 2 == 0) {
+				sI = sI.toLowerCase();
+			}else if (i % 2 == 1) {
+				sI = sI.toUpperCase();
+			}
+			s = s.substring(0, i) + sI + s.substring(i + 1, s.length());
+		}
+		return s;
 	}
 
 }
